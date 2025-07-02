afb7a962d3f8de8b422ffae0ab8467adafa7eebd
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-07-03 01:59:11 +1000

    wip

--- src/loops.ts
+++ src/loops.ts
@@ -22,7 +22,9 @@ export function loops() {
     setInterval(async () => {
         checkHeap();
         try {
-            // global.gc();
+            if (global?.gc) {
+                global.gc();
+            }
         } catch (err) {
 
         }
